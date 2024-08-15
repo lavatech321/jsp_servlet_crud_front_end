9dddb721d2d8eb7cb8fc47509bd42870468e5619
Lavatech <lavatech321>
2024-08-15 21:55:49 +0530

    java beans added

--- src/main/java/com/newtest/bean/EmployeeBean.java
+++ src/main/java/com/newtest/bean/EmployeeBean.java
@@ -1,8 +1,97 @@
 package com.newtest.bean;
+import java.sql.Blob;
 
 public class EmployeeBean {
+	private int id;
+	private String fname;
+	private String lname;
+	private String address;
+	private String username;
+	private String email;
+	private String phno;
+	private String country;
+	private String state;
+	private int zip;
+	private String status;
+	private String jobtype;
+	private Blob pic;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getJobtype() {
+		return jobtype;
+	}
+	public void setJobtype(String jobtype) {
+		this.jobtype = jobtype;
+	}
+	public Blob getPic() {
+		return pic;
+	}
+	public void setPic(Blob pic) {
+		this.pic = pic;
+	}
 	
-	private String name;
-	private String address; 
-
 }
